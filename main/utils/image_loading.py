aa9736a060b07c6da135d11d0294763c61d63f98
Michael Dodds <michaeldodds@pop-os.home>
2025-04-29 12:19:37 +0100

    Fixed Loading

--- main/utils/image_loading.py
+++ main/utils/image_loading.py
@@ -9,22 +9,29 @@ def load_image(filepath: Path, flag = cv2.IMREAD_COLOR_BGR):
     img = cv2.imread(filepath, flags = flag)
     return img 
 
-def load_labeled_images(directorypath: Path) -> Images: 
+def load_labeled_images(directorypath: Path, train: bool = True) -> Images:
+    
     img_paths = list(directorypath.iterdir())
     imgs = [load_image(path) for path in img_paths]
     labels = [directorypath.name] * len(img_paths)  
-    train = directorypath.parent.name == 'train'
-    directory = directorypath.parent.parent.name 
-
+    
     return Images(filepath=img_paths, 
                   labels=labels, 
                   train=train, 
-                  directory=directory, 
+                  directory=directorypath.parent, 
                   imgs=imgs)
 
-def load_directory_images(directorypath: Path) -> Images:
-    labelled_dir_paths = list(directorypath.iterdir())
-    all_img_list = [load_labeled_images(path) for path in labelled_dir_paths]
+def load_directory_images(directorypath: Path, train: bool = True) -> Images:
+    if isinstance(directorypath, str):
+        directorypath = Path(directorypath)
+    
+    if train:
+        directory = directorypath / 'train'
+    else:  
+        directory = directorypath / 'test'
+
+    labelled_dir_paths = list(directory.iterdir())
+    all_img_list = [load_labeled_images(path, train) for path in labelled_dir_paths]
     all_imgs = Images.concat(all_img_list)
     print('Loaded images from:', all_imgs.directory, '\nTraining: ', all_imgs.train)
     return all_imgs
