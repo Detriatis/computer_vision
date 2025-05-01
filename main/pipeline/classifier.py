89c463a323d13f24598d321f3c284afe48758414
mdodds665 <mdodds665@gmail.com>
2025-05-01 11:26:08 +0100

    updated classifiers

--- main/pipeline/classifier.py
+++ main/pipeline/classifier.py
@@ -1,9 +1,37 @@
 from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import NearestNeighbors
+from sklearn.metrics import log_loss
+from skopt import BayesSearchCV
 
+searchspace = { 
+    'supportvectormachines': {
+        'c' : (1, 2)
+                              }, 
+    'randomforest': {
+        'n_estimators': (50 ,500),
+        'max_depth': (5, 50), 
+        'min_samples_split': (2, 20) 
+                     },
 
+    'knn': {
+        'n_neighbours': (2, 30),
+    } 
+}
 
-def fit_model(clf, bows_train, labels_train, bows_test):
-    clf = LinearSVC(C=1.0)   
-    clf.fit(bows_train, labels_train)
-    preds = clf.predict(bows_test)
-    return preds
+models_to_compare = {
+    'supportvectormachines': LinearSVC,
+    'randomforest': RandomForestClassifier,
+    'knn': NearestNeighbors 
+}
+
+def fit_model(clf, train_x, train_y):
+    clf.fit(train_x, train_y)
+
+def pred_model(clf, test_x):
+    preds = clf.predict(test_x)
+
+def model_accuracy(loss_metric, preds, test_y):
+    # Generally the loss metric 
+    # used to fit should be used to evaluate
+    return loss_metric(test_y, preds)  
